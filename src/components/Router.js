07932283c2604867046234356f3471a370194171
jinareum <jinareum.76@gmail.com>
2023-03-19 00:24:30 +0900

    feat : 함수형컴포넌트로 수정후 router호출로 수정

---
+++ src/components/Router.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import {
+    HashRouter as Router,
+    Route,
+    Switch
+} from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+// 인중 로그인 여부에 따라 달라짐
+const AppRouter = () => {
+    const [isLoggedIn,setIsLoggedIn] = useState(false);
+    return(
+        <Router>
+            <Switch>
+                {isLoggedIn ?(
+                    <>
+                    <Route exact path = "/">
+                        <Home/>
+                    </Route>
+                    </>
+                ): (
+                    <Route exact path = "/">
+                        <Auth/>
+                    </Route>
+                    )
+                }
+            </Switch>
+        </Router>
+    )
+}
+
+export default AppRouter;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,3 +1,7 @@
 import React from "react";
 
-export default () => <span>Home</span>;
+const Home = () => (
+    <span>Home</span>
+)
+
+export default Home;
