48bf807fe2052e691bfe32f6bce18c7ee57301a2
jinareum <jinareum.76@gmail.com>
2023-03-24 22:58:57 +0900

    chore : 최초 세팅에 대한 시간차 수정, useEffect 훅 메모

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 
 function App() {
+  const [init, setInit] = useState(false);
   // useState 유저 로그인여부를 체크
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
   return (
       <AppRouter isLoggedIn={isLoggedIn}/>
   );
