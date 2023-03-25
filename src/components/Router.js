96e0282f77d23498c665880c2578e1f09d8e1299
jinareum <jinareum.76@gmail.com>
2023-03-25 17:01:08 +0900

    feat : 주소값만 변경되고 렌더링 이슈 수정

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,25 +1,31 @@
-import React, {useState} from "react";
+import React from "react";
 import {
     HashRouter as Router,
     Route,
     Switch
 } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 // 인중 로그인 여부에 따라 달라짐
 const AppRouter = ({ isLoggedIn }) => {
 
     return(
         <Router>
+            {isLoggedIn && <Navigation/>}
             <Switch>
                 {isLoggedIn ?(
                     <>
-                    <Route exact path = "/">
-                        <Home/>
-                    </Route>
+                        <Route exact path = "/">
+                            <Home/>
+                        </Route>
+                        <Route exact path = "/profile">
+                            <Profile/>
+                        </Route>
                     </>
-                ): (
+                ) : (
                     <Route exact path = "/">
                         <Auth/>
                     </Route>
