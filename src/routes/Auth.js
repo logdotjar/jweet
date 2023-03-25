3486c98f6ed1bca377da71afe75b1d332b419cc0
jinareum <jinareum.76@gmail.com>
2023-03-25 16:28:43 +0900

    feat : 소셜 로그인 기능 구글/깃허브 추가

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,6 +4,9 @@ import { authService } from "fbase";
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    GithubAuthProvider
 } from "firebase/auth";
 
 const Auth = () => {
@@ -52,6 +55,17 @@ const Auth = () => {
     }
 
     const toggleAccount = () => setNewAccount((prev) => !prev);
+    const onSocialClick = async (event) => {
+        const {target : {name}} = event;
+        let provider;
+        if(name == "google"){
+            provider = new GoogleAuthProvider();
+        }else if (name == "github"){
+            provider = new GithubAuthProvider();
+        }
+        const data = await signInWithPopup(authService, provider);
+        console.log(data);
+    }
 
     return (
         <div>
@@ -82,8 +96,14 @@ const Auth = () => {
                 {newAccount ? "가입하기" : "계정 생성하기"}
             </span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button
+                    name="google"
+                    onClick={onSocialClick}
+                >Continue with Google</button>
+                <button
+                    name="github"
+                    onClick={onSocialClick}
+                >Continue with Github</button>
             </div>
         </div>
     )
