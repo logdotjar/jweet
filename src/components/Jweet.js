5e90f6247b49c8c27485d974e1dee47d5aa0cd8f
jinareum <jinareum.76@gmail.com>
2023-03-26 01:58:05 +0900

    feat : 멘션 삭제 기능 함수

--- src/components/Jweet.js
+++ src/components/Jweet.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { dbService } from "fbase";
+import { doc, deleteDoc, updateDoc }from"firebase/firestore";
 
 const Jweet = ({ jweetObj, isOwner }) => {
     // 내가 작성자일 경우 버튼이 보일 수 있게 함
+    const onDeleteJweet = doc(dbService , "jweets", `${jweetObj.id}`)
+    const onDeleteClick = async() => {
+        const ok = window.confirm("정말 삭제하시겠습니까?");
+        if(ok){
+            await deleteDoc(onDeleteJweet);
+        }
+    }
     return(
         <div key={jweetObj.id}>
             <h4>{jweetObj.text}</h4>
             {isOwner &&
                 <>
-                    <button>삭제하기</button>
+                    <button onClick={onDeleteClick}>삭제하기</button>
                     <button>수정하기</button>
                 </>
             }
